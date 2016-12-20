d378f773b634eb005776ae7c430c784348b7b598
Mircea Sorin Sebastian <sorynsoo@gmail.com>
2016-12-20 19:40:11 +0200

    Foarte mari super fixuri

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ base = None
 if sys.platform == "win32":
     base = "Win32GUI"
 
-setup(  name = "MirceaSorin",
-        version = "0.1",
+setup(  name = "MirceaSorin01",
+        version = "1.1",
         description = "Computational Logic Aplication",
         options = {"build_exe": build_exe_options},
         executables = [Executable("MirceaSorin.py", base="console")])
